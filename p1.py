272c1783981d94219155b2ff337376c42211c847
Jaswanth Chennu <chennujaswanth7@gmail.com>
2026-07-10 21:36:28 +0530

    P1

---
+++ p1.py
@@ -0,0 +1,13 @@
+import numpy as np
+#1D Array
+a=np.array([1,2,3,5])
+print(a)
+print(a.ndim)
+#2D Array
+b=np.array([[1,2,3,5],[6,7,8,9,]])
+print(b)
+print(b.ndim)
+#3D Array
+c=np.array([[[1,2,3,5],[6,7,8,9,]],[[5,8,9,5],[7,8,5,4]],[[5,8,3,5],[7,4,5,4]]])
+print(c)
+print(c.ndim)
