b5c6c81c917c88423378f2213b83e5e22c5bc7af
Jaswanth Chennu <chennujaswanth7@gmail.com>
2026-07-13 23:03:45 +0530

    P15

---
+++ p15.py
@@ -0,0 +1,10 @@
+import numpy as np
+#2D Array
+a=np.array([[1,2,3,5],[6,7,8,9]])
+#2D Array
+b=np.array([[1,2,3,5],[6,7,8,9]])
+print(np.exp(a))
+print(np.sqrt(b))
+print(np.sin(a))
+print(np.cos(b))
+print(np.log(a))
